4c299a976a32ac8c6fe18cbe22082bcb79840de1
B-G Seo <bumku1234@gmail.com>
2021-06-01 10:44:06 +0900

    Hash code generation based on DatePicker

--- app/src/main/java/com/example/lotto/ConstellationActivity.kt
+++ app/src/main/java/com/example/lotto/ConstellationActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CalendarView
 import android.widget.DatePicker
 import android.widget.TextView
 import java.text.SimpleDateFormat
@@ -18,6 +19,18 @@ class ConstellationActivity : AppCompatActivity() {
         val datePicker = findViewById<DatePicker>(R.id.datePicker)
         val txtConstell = findViewById<TextView>(R.id.txtConstell)
         txtConstell.text = makeConstellationString(datePicker.month, datePicker.dayOfMonth)
+        val calendar = Calendar.getInstance()
+        datePicker.init(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH),
+        object : CalendarView.OnDateChangeListener, DatePicker.OnDateChangedListener {
+            override fun onSelectedDayChange(p0: CalendarView, p1: Int, p2: Int, p3: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onDateChanged(view: DatePicker?, year: Int, monthOfYear: Int, dayOfMonth: Int) {
+                txtConstell.text = makeConstellationString(datePicker.month, datePicker.dayOfMonth)
+            }
+
+        })
 
         val btnGoResult = findViewById<Button>(R.id.btnGoResultConstell)
         btnGoResult.setOnClickListener {
